eb08e30238f476ed6391ee75e5fd370c86912e86
balddrAdmin <boliklevik@gmail.com>
2025-11-25 22:55:57 +0100

    texts folder

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from keyboards import get_main_keyboard
 
 router = Router()
@@ -10,3 +10,10 @@ router = Router()
 async def start(message: Message):
     keyboard = await get_main_keyboard()
     await message.answer('Witaj!\nJesteśmy Sośnierz Logistic.\nW czym możemy pomóc?', reply_markup=keyboard)
+
+
+
+@router.callback_query(F.data.startswith('contact'))
+async def contact(callback_data: CallbackQuery):
+    with open('texts/kontakt.txt', 'r', encoding='utf-8') as file:
+        await callback_data.message.edit_text(file.read())
--- keyboards.py
+++ keyboards.py
@@ -4,9 +4,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 async def get_main_keyboard():
     buttons = [
         [
-            InlineKeyboardButton(text='Strona główna', callback_data='home'),
-        ],
-        [
             InlineKeyboardButton(text='O nas', callback_data='about'),
             InlineKeyboardButton(text='Oferta', callback_data='offer'),
             InlineKeyboardButton(text='Kariera', callback_data='career'),
