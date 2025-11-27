6977d1a3feeba6b40e52059397cec37f7c723e1c
balddrAdmin <boliklevik@gmail.com>
2025-11-28 00:53:46 +0100

    fsm states fixed

--- handlers.py
+++ handlers.py
@@ -89,28 +89,32 @@ async def write_fsm_name(callback_data: CallbackQuery, state: FSMContext):
 async def write_fsm_email(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     await state.set_state(FSMForm.email)
-    await message.message.edit_text('Proszę podać swój e-mail')
+    await message.answer('Proszę podać swój e-mail')
 
 
 @router.message(FSMForm.email)
 async def write_fsm_number(message: Message, state: FSMContext):
     await state.update_data(email=message.text)
     button_phone = KeyboardButton(text='Wyślij numer', request_contact=True)
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    keyboard.add(button_phone)
+    # Создаем клавиатуру через параметр keyboard
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[[button_phone]],  # список списков кнопок
+        resize_keyboard=True,
+        one_time_keyboard=True
+    )
     await state.set_state(FSMForm.number)
-    await message.message.edit_text('A w tym momencie proszę wpisać swój numer telefonu', reply_markup=keyboard)
+    await message.answer('A w tym momencie proszę wpisać swój numer telefonu', reply_markup=keyboard)
 
 
 @router.message(FSMForm.number)
 async def write_fsm_theme(message: Message, state: FSMContext):
     await state.update_data(number=message.text)
     await state.set_state(FSMForm.theme)
-    await message.message.edit_text('Proszę podać temat zgłoszenia')
+    await message.answer('Proszę podać temat zgłoszenia')
 
 
 @router.message(FSMForm.theme)
 async def write_fsm_text(message: Message, state: FSMContext):
     await state.update_data(theme=message.text)
     await state.set_state(FSMForm.text)
-    await message.message.edit_text('I treść zgłoszenia')
+    await message.answer('I treść zgłoszenia')
